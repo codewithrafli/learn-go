8e420eb07e3208beef4da142de8ff244f284fb9f
xrafffcode <yusfiwawan2@gmail.com>
2025-07-19 00:22:21 +0700

    A.8. Komentar

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	/*
+	   komentar kode
+	   menampilkan pesan hello world
+	*/
 	fmt.Println("Hello", "world!", "how", "are", "you")
+
+	// fmt.Println("baris ini tidak akan dieksekusi")
 }
